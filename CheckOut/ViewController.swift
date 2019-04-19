fbc5316019e5cd2dcb85597e356ec2c5eea6d4ff
SCIS <scis@sciss-macbook-pro.local>
2019-04-19 13:05:01 -0300

    Initial Commit

---
+++ CheckOut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckOut
+//
+//  Created by SCIS on 4/19/19.
+//  Copyright © 2019 Jartola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
